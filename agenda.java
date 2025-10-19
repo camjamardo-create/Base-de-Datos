5d919373ae1c5e2c8e2836c129b250262704ea0f
camjamardo-create <camjamardo@gmail.com>
2025-10-19 13:22:20 -0300

    Implement getters and setters in agenda class
    Added getters and setters for agenda class attributes.

--- agenda.java
+++ agenda.java
@@ -10,4 +10,32 @@ public class agenda {
         this.fecha = fecha;
         this.fecha_libre = fecha_libre;
     }
+
+    // Getters y Setters para leer los atributos privados de la clase desde fuera de ella, y
+    // poder modificarlos si es necesario
+    
+    public int getId_agenda() {
+        return id_agenda;
+    }
+
+    public LocalDateTime getfecha(){
+        return fecha;
+    }
+
+    public boolean getfecha_libre(){
+        return fecha_libre;
+    }
+
+    public void setId_agenda(int id_agenda) {
+        this.id_agenda = id_agenda;
+    }
+
+    public void setfecha(LocalDateTime fecha){
+        this.fecha = fecha;
+    }
+
+    public void setfecha_libre(boolean fecha_libre){
+        this.fecha_libre = fecha_libre;
+    }
 }
+
