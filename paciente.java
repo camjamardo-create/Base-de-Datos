33710f0140105914c4ff382aaa2a7fe2ca2a7b4e
Constanza Quiroga <45968214@alumnos.frsr.utn.edu.ar>
2025-10-19 15:49:02 -0300

    Add getters and setters to paciente class

--- paciente.java
+++ paciente.java
@@ -10,7 +10,63 @@ public class paciente {
     private String obra_social;
     private String domicilio;
     private LocalDate fecha_nacimiento;
-
+    
+//getters y setters
+    public int getDni() {
+        return dni;
+    }
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+    public int getId_paciente() {
+        return id_paciente;
+    }
+    public void setId_paciente(int id_paciente) {
+        this.id_paciente = id_paciente;
+    }
+    public int getEdad() {
+        return edad;
+    }
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getApellido() {
+        return apellido;
+    }
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    public String getTelefono() {
+        return telefono;
+    }
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+    public String getObra_social() {
+        return obra_social;
+    }
+    public void setObra_social(String obra_social) {
+        this.obra_social = obra_social;
+    }
+    public String getDomicilio() {
+        return domicilio;
+    }
+    public void setDomicilio(String domicilio) {
+        this.domicilio = domicilio;
+    }
+    public LocalDate getFecha_nacimiento() {
+        return fecha_nacimiento;
+    }
+    public void setFecha_nacimiento(LocalDate fecha_nacimiento) {
+        this.fecha_nacimiento = fecha_nacimiento;
+    }
+    
     // Constructor
     public paciente(int dni, int id_paciente, int edad, String nombre, String apellido, String telefono, String obra_social, String domicilio, LocalDate fecha_nacimiento) {
         this.dni = dni;
@@ -25,3 +81,4 @@ public class paciente {
     }
     
 }
+
