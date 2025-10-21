6bbb6dc3c44c2052ec5846af291ad0066be0b9c4
Constanza Quiroga <45968214@alumnos.frsr.utn.edu.ar>
2025-10-21 10:21:00 -0300

    Refactor insertarPaciente method and SQL query

--- pacienteDAO.java
+++ pacienteDAO.java
@@ -1,5 +1,4 @@
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.PreparedStatement; // Importa la clase PreparedStatement para ejecutar sentencias SQL precompiladas
 
@@ -16,17 +15,19 @@ public class pacienteDAO {
     }
     // crear paciente
     public void insertarPaciente(paciente pac) throws SQLException {
-        String sql = "INSERT INTO paciente (dni, edad, nombre, apellido, telefono, obra_social, domicilio, fecha_nacimiento) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+        String sql = "INSERT INTO paciente (dni, nombre, apellido, domicilio, telefono, fecha_nac, obra_social, genero, contrasena) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
         PreparedStatement stmt = connection.prepareStatement(sql);
         stmt.setInt(1, pac.getDni());
-        stmt.setInt(2, pac.getEdad());
-        stmt.setString(3, pac.getNombre());
-        stmt.setString(4, pac.getApellido());
+        stmt.setString(2, pac.getNombre());
+        stmt.setString(3, pac.getApellido());
+        stmt.setString(4, pac.getDomicilio());
         stmt.setString(5, pac.getTelefono());
-        stmt.setString(6, pac.getObra_social());
-        stmt.setString(7, pac.getDomicilio());
-        stmt.setDate(8, java.sql.Date.valueOf(pac.getFecha_nacimiento()));
+        stmt.setDate(6, java.sql.Date.valueOf(pac.getFecha_nacimiento()));
+        stmt.setString(7, pac.getObra_social());
+        stmt.setString(8, pac.getGenero());
+        stmt.setString(9, pac.getContraseña());
         stmt.executeUpdate();
     }
 
 }
+
