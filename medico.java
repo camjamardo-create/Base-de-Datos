3be1a3df6d6d9b78f794ee659a4ef4c72e917294
camjamardo-create <camjamardo@gmail.com>
2025-10-21 10:22:15 -0300

    Update medico.java

--- medico.java
+++ medico.java
@@ -3,13 +3,17 @@ public class medico {       // Definimos atributos
     private String nombre;
     private String apellido;
     private String especialidad;
+    private int DNI;
+    private String contraseña;
 
     // Constructor
-    public medico(int id_medico, String nombre, String apellido, String especialidad) {
+    public medico(int id_medico, String nombre, String apellido, String especialidad, int DNI, String contraseña) {
         this.id_medico = id_medico;
         this.nombre = nombre;
         this.apellido = apellido;
         this.especialidad = especialidad;
+        this.DNI = DNI;
+        this.contraseña = contraseña;
     }
 
     // Getters y Setters 
@@ -27,6 +31,14 @@ public class medico {       // Definimos atributos
         return especialidad;
     }
 
+    public int getDni() {
+        return DNI;
+    }
+    public String getContraseña() {
+        return contraseña;
+    }
+
+
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
@@ -37,6 +49,15 @@ public class medico {       // Definimos atributos
     public void setEspecialidad(String especialidad) {
         this.especialidad = especialidad;
     }
+
+    public void setDni(int dni) {
+        this.DNI = DNI;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
 }
+
 
 
