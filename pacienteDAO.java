5c3bfcc4b9796361c7667326f09051fbb82befef
Constanza Quiroga <45968214@alumnos.frsr.utn.edu.ar>
2025-10-23 15:00:40 -0300

    Add methods to read and update patients

--- pacienteDAO.java
+++ pacienteDAO.java
@@ -28,6 +28,60 @@ public class pacienteDAO {
         stmt.setString(9, pac.getContraseña());
         stmt.executeUpdate();
     }
+    // leer pacientes
+    public List<paciente> obtenerPacientes() {
+        List<paciente> pacientes = new ArrayList<>();
+        try {
+            String sql = "SELECT * FROM paciente";
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()){
+                int id_paciente = rs.getInt("id_paciente");
+                int dni = rs.getInt("dni");
+                String nombre = rs.getString("nombre");
+                String apellido = rs.getString("apellido");
+                String domicilio = rs.getString("domicilio");
+                String telefono = rs.getString("telefono");
+                LocalDate fecha_nac = rs.getDate("fecha_nac").toLocalDate();
+                String obra_social = rs.getString("obra_social");
+                String genero = rs.getString("genero");
+                String contrasena = rs.getString("contrasena");
+                pacientes.add(new paciente(id_paciente, dni, nombre, apellido, domicilio, telefono, obra_social, fecha_nac, genero, contrasena));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace(); // print stack trace on error sirve para imprimir el seguimiento de la pila en caso de error
+        }   
+        return pacientes;
+    }
+    //Actualizar pacientes
+    public void actualizarPaciente(paciente pac) throws SQLException {
+        String sql = "UPDATE paciente SET dni=?, nombre=?, apellido=?, domicilio=?, telefono=?, fecha_nac=?, obra_social=?, genero=?, contrasena=? WHERE id_paciente=?";
+        PreparedStatement stmt = connection.prepareStatement(sql);
+        stmt.setInt(1, pac.getDni());
+        stmt.setString(2, pac.getNombre());
+        stmt.setString(3, pac.getApellido());
+        stmt.setString(4, pac.getDomicilio());
+        stmt.setString(5, pac.getTelefono());
+        stmt.setDate(6, java.sql.Date.valueOf(pac.getFecha_nacimiento()));
+        stmt.setString(7, pac.getObra_social());
+        stmt.setString(8, pac.getGenero());
+        stmt.setString(9, pac.getContraseña());
+        stmt.setInt(10, pac.getId_paciente());
+        stmt.executeUpdate();
+    }
 
+    // cerrar conexion
+    public void cerrarConexion() {
+        try {
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+                System.out.println("Conexión cerrada desde pacienteDAO");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
 
